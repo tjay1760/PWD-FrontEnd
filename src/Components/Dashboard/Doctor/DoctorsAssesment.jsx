315e42b383a378780c76c8340c656103ca11fcc9
tjay1760 <mcjthiongo@gmail.com>
2025-06-08 15:58:42 +0300

    Doctors assesment

--- src/Components/Dashboard/Doctor/DoctorsAssesment.jsx
+++ src/Components/Dashboard/Doctor/DoctorsAssesment.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
-import { DisabilityAssessmentChart } from "./Doctors_chart";
+import AssessmentsTable from '../AssesmentsTable';
 import { Button } from '../../../components/ui/button';
 import { HeartPlus } from 'lucide-react'; // Assuming you have a HeartPlus icon in lucide-react
+import toast from 'react-hot-toast';
 const DoctorsAssesment = () => {
     const handleClick = () => {
-    // Implement what happens when the button is clicked
-    // For example, opening a modal for booking, or navigating to a new page.
-    console.log("Add Appointment button clicked!");
+
     // You might want to show a toast notification:
-    // toast.info("Adding a new appointment...");
+    toast("Creating Appointment...");
   };
   return (
     <div className="assesments-page">
@@ -25,6 +24,16 @@ const DoctorsAssesment = () => {
         <HeartPlus />
       <span>ADD APPOINTMENT</span>
     </Button>
+    <div className="tables flex gap-6 border rounded-lg p-6">
+        <div className="table-container">
+            <h2 className="text-2xl font-bold mb-4">Doctors Assessments</h2>
+            <AssessmentsTable />
+        </div>
+                <div className="table-container">
+            <h2 className="text-2xl font-bold mb-4">PWD Flagged for Reviewgit</h2>
+            <AssessmentsTable />
+        </div>
+    </div>
         </div>
   )
 }
